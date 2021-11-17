086fe82bf6a8c3c5851193755871cb84f226ad62
stdierckx <st.dierckx@gmail..com>
2021-11-17 11:34:31 +0100

    improving setup

--- save-config.sh
+++ save-config.sh
@@ -2,4 +2,6 @@
 cp -r ~/.config/bspwm .
 cp -r ~/.config/sxhkd .
 
+cp ~/.Xresources .
+
 notify-send 'Configuration' 'Configuration saved to the config files git repo'
--- setup.sh
+++ setup.sh
@@ -3,14 +3,20 @@
 echo "Thalos dev environment installer " && sleep 5
 echo "Installing Dependencies ... "
 
-apt install bspwm sxhkd rofi zsh
+apt install bspwm sxhkd rxvt-unicode feh rofi zsh curl
 
 echo "Copying config ... "
 
 cp -r bspwm ~/.config
 cp -r sxhkd ~/.config
+cp .Xresources ~
 
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/sxhkd/sxhkdrc
 
 cp -r wallpapers/ ~/.wallpapers
+
+
+ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ sudo snap install spotify
