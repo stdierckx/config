7dcbf471e8cb2f673980cbae1e5474dabdf58e4e
stdierckx <st.dierckx@gmail..com>
2021-11-17 11:37:31 +0100

    improving setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "Thalos dev environment installer " && sleep 5
 echo "Installing Dependencies ... "
 
-apt install bspwm sxhkd rxvt-unicode feh rofi zsh curl
+apt install bspwm sxhkd rxvt-unicode feh rofi zsh curl vim
 
 echo "Copying config ... "
 
@@ -20,3 +20,8 @@ cp -r wallpapers/ ~/.wallpapers
  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
  sudo snap install spotify
+
+git config --global user.email 'st.dierckx@gmail..com'
+git config --global user.name stdierckx
+git config --global credential.helper store
+
